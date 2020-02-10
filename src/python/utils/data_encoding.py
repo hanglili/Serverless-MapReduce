cbcb735aa13ecf3a7d3f991ff9c1e0e87810cb80
Hang Li Li <hang1997li@gmail.com>
2020-02-10 15:06:21 +0000

    Implemented shuffling

--- src/python/utils/data_encoding.py
+++ src/python/utils/data_encoding.py
@@ -5,6 +5,7 @@ from aws_xray_sdk.core import xray_recorder
 JOB_INFO = "configuration/job-info.json"
 MAP_HANDLER = "job/map_handler.py"
 REDUCE_HANDLER = "job/reduce_handler.py"
+PARTITION = "job/partition.py"
 UTILS_INIT_FILE = "job/__init__.py"
 JOB_INIT_FILE = "utils/__init__.py"
 
@@ -12,6 +13,7 @@ JOB_INIT_FILE = "utils/__init__.py"
 @xray_recorder.capture('zip_lambda')
 def zip_lambda(fname, zipname):
     # faster to zip with shell exec
-    subprocess.call(['zip', zipname] + glob.glob(fname) + glob.glob(JOB_INFO) +
-                    glob.glob("utils/lambda_utils.py") + glob.glob(MAP_HANDLER)
-                    + glob.glob(REDUCE_HANDLER) + glob.glob(JOB_INIT_FILE) + glob.glob(UTILS_INIT_FILE))
+    subprocess.call(['zip', zipname] + glob.glob(fname) + glob.glob(JOB_INFO)
+                    + glob.glob("utils/lambda_utils.py") + glob.glob(MAP_HANDLER)
+                    + glob.glob(REDUCE_HANDLER) + glob.glob(JOB_INIT_FILE) + glob.glob(UTILS_INIT_FILE)
+                    + glob.glob(PARTITION))
