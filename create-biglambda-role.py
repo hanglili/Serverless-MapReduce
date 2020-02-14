87200009349eea5d338a9d54d4c8b3184c62a196
Hang Li Li <hang1997li@gmail.com>
2020-02-14 00:59:15 +0000

    Updated create-biglambda-role.py and delete-biglambda-role.py to python 3.7

--- create-biglambda-role.py
+++ create-biglambda-role.py
@@ -1,4 +1,7 @@
-import boto3,json,botocore
+import boto3
+import botocore
+import json
+
 client = boto3.client('iam')
 
 trust_role = {
@@ -19,7 +22,7 @@ rn='serverless_mr_role'
 rp='serverless_mr_policy'
 
 try:
-    response = client.create_role(RoleName=rn,AssumeRolePolicyDocument=json.dumps(trust_role))
+    response = client.create_role(RoleName=rn, AssumeRolePolicyDocument=json.dumps(trust_role))
     print(response['Role']['Arn'])
     print("Success: done creating role")
 except botocore.exceptions.ClientError as e:
