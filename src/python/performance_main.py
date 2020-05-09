8bc10f5aeb0bff2728d06563cb0e6f8d9d823073
Hang Li Li <hang1997li@gmail.com>
2020-05-09 12:16:20 +0100

    Added all the jobs for evaluation

--- src/python/performance_main.py
+++ src/python/performance_main.py
@@ -1,8 +1,33 @@
 from serverless_mr.main import ServerlessMR
 
-from performance_functions.query_2_map import map_function
-from performance_functions.query_2_reduce import reduce_function
-
 serverless_mr = ServerlessMR()
-serverless_mr.map(map_function).reduce(reduce_function, 4).run()
 
+# query 1
+# from performance_functions.query_1 import map_function
+# serverless_mr.map(map_function).run()
+#
+# query 2
+# from performance_functions.query_2 import map_function, reduce_function
+# serverless_mr.map(map_function).reduce(reduce_function, 4).run()
+#
+# query 2b
+# from performance_functions.query_2_var import map_function, reduce_function
+# serverless_mr.map(map_function).reduce(reduce_function, 4).run()
+#
+# query 3
+from performance_functions.query_3_phase_1 import map_function_1, reduce_function_1
+from performance_functions.query_3_phase_2 import map_function_2, reduce_function_2
+from performance_functions.query_3_phase_3 import map_function_3, reduce_function_3
+serverless_mr.map(map_function_1).reduce(reduce_function_1, 4)
+serverless_mr.map(map_function_2).reduce(reduce_function_2, 4)
+serverless_mr.map(map_function_3).reduce(reduce_function_3, 1)
+serverless_mr.run()
+
+# Simplified Page Rank
+# from performance_functions.simplified_page_rank import map_function, reduce_function
+# serverless_mr.map(map_function).reduce(reduce_function, 4).run()
+
+# Sort
+# from performance_functions.sorting import map_function, reduce_function
+# from performance_functions.range_partition import partition
+# serverless_mr.map(map_function).shuffle(partition).reduce(reduce_function, 4).run()
